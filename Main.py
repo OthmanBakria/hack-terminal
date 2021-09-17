9a0f96af93ad52477b716c6a8e3a0233002c1c20
kamalil <88183528+kamalil@users.noreply.github.com>
2021-09-17 15:28:51 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,22 @@
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+cyan="\033[1;36m"
+violate="\033[1;37m"
+nc="\033[00m"
+print(f'''\007
+	{yellow}
+ h      h hh       ccccccc  k    k
+ h      h h h      c        k   k
+ h      h h  h     c        k  k
+ hhhhhhh  h   h    c        kkk
+ h      h hhhhhh   c        k  k
+ h {red}     h h     h  c        k   k
+ h      h h      h ccccccc  k    k
+{nc}''')
+x = input('enter neumber phone ')
+print('please wait')
+
+
